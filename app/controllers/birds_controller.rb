7fa02581d72da08938d3c5d20d4b1751e32843d8
Sabiha Bary <sabihaab9@gmail.com>
2022-08-07 13:22:53 -0400

    commit tree 3b9df7e8bba9d6a237013ce149556d42d2d8b834

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -14,44 +14,51 @@ class BirdsController < ApplicationController
 
   # GET /birds/:id
   def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    #bird = Bird.find_by(id: params[:id])
+    bird = find_bird
+    render json: bird
+    rescue ActiveRecord::RecordNotFound
+      #render json: { error: "Bird not found" }, status: :not_found
+      render_not_found_response
+
   end
 
-  # PATCH /birds/:id
+  #PATCH /birds/:id
   def update
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(bird_params)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    #bird = Bird.find_by(id: params[:id])
+    bird = find_bird
+    bird.update(bird_params)
+    render json: bird
+    
+    rescue ActiveRecord::RecordNotFound
+    #render json: { error: "Bird not found" }, status: :not_found
+      render_not_found_response
   end
 
   # PATCH /birds/:id/like
   def increment_likes
-    bird = Bird.find_by(id: params[:id])
+    #bird = Bird.find_by(id: params[:id])
+    bird = find_bird
+
     if bird
       bird.update(likes: bird.likes + 1)
       render json: bird
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      #render json: { error: "Bird not found" }, status: :not_found
+      render_not_found_response
     end
   end
 
   # DELETE /birds/:id
   def destroy
-    bird = Bird.find_by(id: params[:id])
+    #bird = Bird.find_by(id: params[:id])
+    bird = find_bird
     if bird
       bird.destroy
       head :no_content
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      #render json: { error: "Bird not found" }, status: :not_found
+      render_not_found_response
     end
   end
 
@@ -59,6 +66,19 @@ class BirdsController < ApplicationController
 
   def bird_params
     params.permit(:name, :species, :likes)
+  end
+
+  #private helper method for generating the :not_found response
+  def render_not_found_response
+    render json: {error: "Bird not found"}, status: :not_found 
+  end
+
+  #private helper method to find a bird based on the ID in the params hash
+  def find_bird
+     #Bird.find_by(id: params[:id]) -> returns 'nil' if record isnt found
+     Bird.find(params[:id])
+     #we will get an ActiveRecord::RecordNotFound exception instead of nil 
+     #when the record doesn't exist. 
   end
 
 end
